a3901d36b3763f28eb8d4016d32a99e1d9b35d76
Bastien Vaysse <bastien.vaysse@supagro.inra.fr>
2013-02-19 14:05:29 +0100

    Adding missing files to PyOpenFLUID structure
    * Added __init__.py
    * Modified gitignore to accept files beginning by "__"

---
+++ module/__init__.py
@@ -0,0 +1,15 @@
+#!/bin/env/python
+# -*- coding: UTF-8 -*-
+
+"""OpenFLUID package for Python2."""
+
+__author__ = "Bastien Vaysse"
+__copyright__ = "Copyright 2013, Creative Commons By-NC-ND license"
+__credits__ = ["Bastien VAYSSE", "Jean-Christophe FABRE"]
+__license__ = "GPLv3"
+__version__ = "2.0.0~alpha"
+__email__ = "bastien.vaysse@supagro.inra.fr"
+__status__ = "Production"
+
+from PyOpenFLUID import PyOpenFLUID
+from PyOFExceptions import PyOFError
