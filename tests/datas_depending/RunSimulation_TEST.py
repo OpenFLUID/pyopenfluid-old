af5442dae17659cf44d13b9b2fd841e5fe38ac4c
Bastien Vaysse <bastien.vaysse@supagro.inra.fr>
2013-03-01 16:16:55 +0100

    Added test on runSimulation function

---
+++ tests/datas_depending/RunSimulation_TEST.py
@@ -0,0 +1,21 @@
+#!/bin/env/python
+# -*- coding: UTF-8 -*-
+
+from BaseTest import *
+
+class PyOpenFLUIDTest_RunSimulation(PyOpenFLUIDTest):
+
+    def mainTest(self):
+        """Test of project functions."""
+        self.assertEquals(len(ArgList), 1)
+        Path = ArgList[0]
+
+        self.checkDirectory(Path)
+        Res = self.runProject(Path)
+
+        self.assertTrue(isinstance(Res, bool))
+        self.assertTrue(Res)
+
+if __name__ == "__main__":
+  ArgList = skipArgFromLC()
+  unittest.main()
