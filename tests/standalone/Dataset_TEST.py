1b642a81e2d4b7666f52540815819cc1330948dc
Bastien Vaysse <bastien.vaysse@supagro.inra.fr>
2013-03-25 15:45:46 +0100

    Adding new tools test
    * Added in OpenFLUIDConfig custom check, load and clear function on simulation config
    * Updated test of Dataset with new tools

--- tests/standalone/Dataset_TEST.py
+++ tests/standalone/Dataset_TEST.py
@@ -2,22 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 from BaseTest import *
-from random import randint, random
-
-def randbool (nb=2) :
-    assert nb > 0, "randbool impossible"
-    return random() < (1.0 / nb)
-
-def popRandom (list_, avoid=[]):
-    a = list_[randint(0, len(list_)-1)]
-    while a in avoid:
-        a = list_[randint(0, len(list_)-1)]
-    return a
-
-def clear(pyof):
-    pyof.clearAllUnits()
-    pyof.clearModel()
-    pyof.clearMonitoring()
+from OpenFLUIDConfig import *
 
 class PyOpenFLUIDTest_OpenDataSet(PyOpenFLUIDTest):
 
@@ -26,79 +11,46 @@ class PyOpenFLUIDTest_OpenDataSet(PyOpenFLUIDTest):
         DSPath = ArgList.get("dataset", None)
         self.assertIsNotNone(DSPath)
 
-        # clearing datas
-        clear(self.openfluid)
-        # adding datas to dataset
-        LIST_UNIT = dict(
-            UnitClassTest = [(i, randint(1,20)) for i in range(8)],
-            SecClass = [(i, randint(1,20)) for i in range(10, 20)] )
-        LIST_FUNCTIONS = dict(funcA = dict(zero="0"), funcB = dict(),
-            funcC = dict(one="one", two="two", three="three"))
-        LIST_OBSERVERS = dict(genA = dict(paramOne="one", paramTwo="two"), genB = dict())
-        LIST_PARENT_CHILD_RELATION = dict()
-        LIST_FROM_TO_RELATION = dict()
+        # clearing datas AND settings new datas
+        Config = dict(\
+            begin_date = "2012-02-01 00:20:10",\
+            end_date = "2013-10-10 10:10:10",\
+            delta_t = 3600,\
+            model_global_param = [("bluesky", "yes"), ("redstone", "no"), ("zero", "0")],\
+            extra_function_path = ["the/function/path", "an/other/function/path"],\
+            extra_observer_path = ["the/observer/path", "an/other/observer/path"],\
+            functions = [ ("funcA", [("fixedvalue","3"), ("minvalue", "-5")]),\
+                ("funcB", [("rangemin", "0"), ("rangemax", "5"), ("rangeincr", "0.1")]),\
+                ("funcC", [("empty", "10"), ("random", "0.1454")]) ],\
+            observers = [ ("obsA", [("format.h1.header","title")]),\
+                ("obsB", [("column.f1.maxsize", "20"), ("column.f1.minsize", "0")]),\
+                ("obsC", []) ],\
+            units = [("SU", 1, 1), ("SU", 2, 2), ("SU", 3, 3), ("SU", 4, 4), ("SU", 5, 5),\
+                ("DD", 1, 1), ("DD", 2, 1), ("DD", 3, 1), ("DD", 4, 1)],\
+            from_to = [ ("SU", 1, [("SU", 2), ("SU", 3)]), ("SU", 5, [("SU", 4)]),\
+                ("SU", 4, [("DD", 1)]), ("DD", 2, [("DD", 3)]) ],\
+            parent_child = [ ("SU", 1, [("DD", 1), ("DD", 2)]), ("DD", 1, [("SU", 2)]) ],\
+            input_data = [ ("SU", "area", "0"), ("SU", "color", "blue"),\
+                ("DD", "unknown", "none") ],\
+            input_data_by_units = [("SU", 1, [("area", "100.0"), ("color", "red")]),\
+                ("SU", 2, [("color", "green")]), ("DD", 1, [("unknown", "true")]),\
+                ("DD", 2, [("unknown", "true")]) ]
+        )
 
-        for nmFun, paramFun in LIST_FUNCTIONS.items():
-            self.openfluid.addFunction(nmFun)
-            for nmParam, vlParam in paramFun.items():
-                self.openfluid.setFunctionParam(nmFun, nmParam, vlParam)
-
-        for nmObs, paramObs in LIST_OBSERVERS.items():
-            self.openfluid.addObserver(nmObs)
-            for nmParam, vlParam in paramObs.items():
-                self.openfluid.setObserverParam(nmObs, nmParam, vlParam)
-
-        for unitClass, lUnit in LIST_UNIT.items():
-            for unitID, unitPO in lUnit:
-                self.openfluid.addUnit(unitClass, unitID, unitPO)
-
-        for unitClass, lUnit in LIST_UNIT.items():
-            LIST_PARENT_CHILD_RELATION[unitClass] = dict()
-            LIST_FROM_TO_RELATION[unitClass] = dict()
-            _tmp_LISTID = map((lambda (a,b):a), lUnit)
-
-            for unitID, unitPO in lUnit:
-                LIST_PARENT_CHILD_RELATION[unitClass][unitID] =\
-                    [i for i in _tmp_LISTID if i != unitID and randbool(3)]
-                LIST_FROM_TO_RELATION[unitClass][unitID] =\
-                    popRandom(_tmp_LISTID, avoid=[unitID])
-
-                for childID in LIST_PARENT_CHILD_RELATION[unitClass][unitID]:
-                    self.openfluid.addParentChildConnection(unitClass, unitID, unitClass, childID)
-                self.openfluid.addFromToConnection(unitClass, unitID, unitClass,
-                    LIST_FROM_TO_RELATION[unitClass][unitID])
-
-        self.openfluid.setPeriodBeginDate("2001-01-01 01:01:01")
-        self.openfluid.setPeriodEndDate("2002-02-02 02:02:02")
-        self.openfluid.setDefaultDeltaT(3600)
+        clearOpenFLUIDconfig(self.openfluid)
+        loadOpenFLUIDConfig(Config, self.openfluid)
+        checkOpenFLUIDConfig(Config, self.openfluid)
 
         # tests of saveDataset
         self.openfluid.saveDataset(DSPath)
-        clear(self.openfluid)
+        clearOpenFLUIDconfig(self.openfluid)
 
         # tests of openDataset
         self.checkDirectory(DSPath)
         self.openfluid.openDataset(DSPath)
 
         # tests of content
-        self.assertItemsEqual(LIST_FUNCTIONS.keys(), self.openfluid.getFunctionsInModel())
-        for nmFun, paramFun in LIST_FUNCTIONS.items():
-            for nmParam, vlParam in paramFun.items():
-                self.assertEquals(self.openfluid.getFunctionParam(nmFun, nmParam), vlParam)
-
-        self.assertItemsEqual(LIST_OBSERVERS.keys(), self.openfluid.getObserversInMonitoring())
-        for nmObs, paramObs in LIST_OBSERVERS.items():
-            for nmParam, vlParam in paramObs.items():
-                self.assertEquals(self.openfluid.getObserverParam(nmObs, nmParam), vlParam)
-
-        self.assertItemsEqual(LIST_UNIT.keys(), self.openfluid.getUnitsClasses())
-        for unitClass, lUnit in LIST_UNIT.items():
-            for unitID, unitPO in lUnit:
-                self.assertEquals(unitPO, self.openfluid.getProcessOrder(unitClass, unitID))
-            self.assertEquals(self.openfluid.getUnitTos(unitClass, unitID),
-                [(unitClass, LIST_FROM_TO_RELATION[unitClass][unitID])])
-            self.assertItemsEqual(self.openfluid.getUnitChildren(unitClass, unitID),
-                [(unitClass, a) for a in LIST_PARENT_CHILD_RELATION[unitClass][unitID] ])
+        checkOpenFLUIDConfig(Config, self.openfluid)
 
 if __name__ == "__main__":
     ArgList = skipArgFromCL()
