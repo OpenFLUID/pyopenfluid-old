680b7c2c9ffe7cc62be34350b259ff0ecd82e20c
Bastien Vaysse <bastien.vaysse@supagro.inra.fr>
2013-03-26 11:39:00 +0100

    Added optional parameter render in doxygen
    * Added aliases for creating render (on the model of example aliases)
    * Added optional parameters in saveProject doc
    * Modified doc build custom system with new aliases

--- DocStrings.cmake
+++ DocStrings.cmake
@@ -576,10 +576,10 @@ SET(DOCSTRINGRAW_RETURN_SAVEPROJECT "")
 SET(DOCSTRINGRAW_PARAMETERS_SAVEPROJECT
     "path        -- the output directory"
     "kw          -- optional keyword values")
-SET(DOCSTRINGRAW_OPTIONAL_PARAMETERS_SAVEPROJECT)
-#    "name        -- the name of the project"
-#    "description -- a short description of the project"
-#    "authors     -- a string of author(s)")
+SET(DOCSTRINGRAW_OPTIONAL_PARAMETERS_SAVEPROJECT
+    "name        -- the name of the project"
+    "description -- a short description of the project"
+    "authors     -- a string of author(s)")
 SET(DOCSTRINGRAW_EXAMPLES_SAVEPROJECT "")
 
 
